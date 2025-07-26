c3bd7ccd27b7157f3f76626e923683aa2f34c826
Rufus Bright <rufusbright595@gmail.com>
2025-07-26 22:32:04 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -50,7 +50,7 @@
            className="relative mb-4 h-48 w-48 sm:h-64 sm:w-64"
          >
             <Image
-              src="/assets/logo.png"
+              src="/assets/img/Furniture.png"
               alt="Crafting Corner logo"
               width={512}
               height={512}
